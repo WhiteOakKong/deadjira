c4322bcbe5e3e1c0f33a456173284bcf9084a403
WhiteOakKong <aadam.debevec@outlook.com>
2023-06-24 10:33:58 -0500

    Add test files and deployment script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,13 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.19;
 
-import { Foo } from "../src/Foo.sol";
+import { DeadjiraAuction } from "../src/Deadjira.sol";
 
 import { BaseScript } from "./Base.s.sol";
 
-/// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/tutorials/solidity-scripting
 contract Deploy is BaseScript {
-    function run() public broadcaster returns (Foo foo) {
-        foo = new Foo();
+    function run() public broadcaster returns (DeadjiraAuction auction) {
+        auction = new DeadjiraAuction();
     }
 }
